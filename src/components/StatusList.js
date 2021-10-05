e779e191515a52f6cd8570e3b680c08bf610bfaa
G7M7T1 <genlisense@gmail.com>
2021-10-05 12:03:46 -0400

    Fix Status
    Fix Status and RWD

--- src/components/StatusList.js
+++ src/components/StatusList.js
@@ -65,6 +65,84 @@ function StatusList() {
               <p className="WorkingTag">Working</p>
             </div>
             <div className="productStatus">
+              <p>Ring</p>
+              <p className="WorkingTag">Working</p>
+            </div>
+            <div className="productStatus">
+              <p>Exodus</p>
+              <p className="WorkingTag">Working</p>
+            </div>
+            <div className="productStatus">
+              <p>NE</p>
+              <p className="WorkingTag">Working</p>
+            </div>
+            <div className="productStatus">
+              <p>Brush</p>
+              <p className="WorkingTag">Working</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="statusCard">
+          <img src="/status1.png" alt="Status" />
+          <div className="statusCardList">
+            <div className="productStatus">
+              <p>Dash</p>
+              <p className="WorkingTag">Working</p>
+            </div>
+            <div className="productStatus">
+              <p>Ring</p>
+              <p className="WorkingTag">Working</p>
+            </div>
+            <div className="productStatus">
+              <p>Exodus</p>
+              <p className="WorkingTag">Working</p>
+            </div>
+            <div className="productStatus">
+              <p>NE</p>
+              <p className="WorkingTag">Working</p>
+            </div>
+            <div className="productStatus">
+              <p>Brush</p>
+              <p className="WorkingTag">Working</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="statusCard">
+          <img src="/status1.png" alt="Status" />
+          <div className="statusCardList">
+            <div className="productStatus">
+              <p>Dash</p>
+              <p className="WorkingTag">Working</p>
+            </div>
+            <div className="productStatus">
+              <p>Ring</p>
+              <p className="WorkingTag">Working</p>
+            </div>
+            <div className="productStatus">
+              <p>Exodus</p>
+              <p className="WorkingTag">Working</p>
+            </div>
+            <div className="productStatus">
+              <p>NE</p>
+              <p className="WorkingTag">Working</p>
+            </div>
+            <div className="productStatus">
+              <p>Brush</p>
+              <p className="WorkingTag">Working</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="statusCard">
+          <img src="/status1.png" alt="Status" />
+          <div className="statusCardList">
+            <div className="productStatus">
+              <p>Dash</p>
+              <p className="WorkingTag">Working</p>
+            </div>
+            <div className="productStatus">
               <p>Dash</p>
               <p className="WorkingTag">Working</p>
             </div>
